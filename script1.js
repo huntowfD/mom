faaa1dc269054540260953c122efc6e1b61011a7
unknown <0002oahtcom@gmail.com>
2025-03-07 10:03:40 +0700

    mom

---
+++ script1.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('#image img');
+
+    image.addEventListener('click', function() {
+        alert('Con yêu mẹ nhiều lắm!'); // Thông điệp yêu thương
+    });
+});
